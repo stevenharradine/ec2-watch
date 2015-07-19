900446c19084e441ae3637230d07713455126de8
steven harradine <steven.harradine@telus.com>
2015-07-18 23:28:19 -0400

    remove ,

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-module.exports.SLACK_TOKEN               = "TOKEN/TOKEN/TOKEN",
+module.exports.SLACK_TOKEN               = "TOKEN/TOKEN/TOKEN"
 module.exports.ENVIROMENT_AWS_REGION_MAP = require("./enviromentAwsRegionMap.json")
 module.exports.AIR_TRAFFIC_CONTROL_URL   = "http://localhost:3000/"
 module.exports.CURRENT_ENVIROMENT        = "next"
